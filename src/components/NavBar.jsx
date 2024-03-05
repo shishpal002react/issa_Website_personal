cf44054a0ce5587131cbfcb46b7b872d55a86b3f
Flutter05 <flutter05@flyweis.technology>
2024-03-05 19:41:10 +0530

    update

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -80,7 +80,7 @@ export const NavBar = () => {
                   id="collapsible-nav-dropdown"
                   className="custom-dropdown-title"
                 >
-                  <NavDropdown.Item href="https://65e5cd7361653525e6bb4cfd--hilarious-ganache-1743e6.netlify.app/">
+                  <NavDropdown.Item href="https://65e724979ce37c1f05e04b02--hilarious-ganache-1743e6.netlify.app/">
                     Patient Panel
                   </NavDropdown.Item>
                   <NavDropdown.Item href="https://issa-employee-panel.vercel.app/">
